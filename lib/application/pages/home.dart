495b20b0a118d260ac40c967a1321e187c8efa84
Nadia Islam <sohasara853@gmail.com>
2025-02-13 19:51:56 +0600

    add app bar to home page and disable debug banner

--- lib/application/pages/home.dart
+++ lib/application/pages/home.dart
@@ -5,6 +5,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('To Do App'),
+      ),
+    );
   }
 }
