e4b20bcf7d989ea2d96e5b8205426b2b83e8686e
Nadia Islam <sohasara853@gmail.com>
2025-02-14 13:06:03 +0600

    add floating action button and update app bar color in HomePage

--- lib/application/pages/home.dart
+++ lib/application/pages/home.dart
@@ -6,7 +6,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: const CircleBorder(),
+        backgroundColor: Colors.indigo.shade300,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
       appBar: AppBar(
+        backgroundColor: Colors.indigo.shade300,
         centerTitle: true,
         title: const Text('To Do App'),
       ),
