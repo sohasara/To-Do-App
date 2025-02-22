ecbba1581f220d64ab49f3bd0fb9fb61b2b60d4e
Nadia Islam <sohasara853@gmail.com>
2025-02-22 22:40:32 +0600

    implement ListView for displaying completed tasks in CompTask page

--- lib/application/pages/completed_task/comp_task.dart
+++ lib/application/pages/completed_task/comp_task.dart
@@ -6,17 +6,35 @@ class CompTask extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.indigo.shade300,
-        centerTitle: true,
-        title: const Text(
-          'Completed Tasks',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.indigo.shade300,
+          centerTitle: true,
+          title: const Text(
+            'Completed Tasks',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
         ),
-      ),
-    );
+        body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 10.0, left: 10, right: 10),
+                child: Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.indigo.shade100,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ListTile(
+                    title: Text('Task $index'),
+                    subtitle: Text('Description of Task $index'),
+                    trailing: const Text('Date: 12/12/2021'),
+                  ),
+                ),
+              );
+            }));
   }
 }
