75982ef72329adb6f6879a299a34d6ead81ca99a
Nadia Islam <sohasara853@gmail.com>
2025-02-27 10:20:00 +0600

    refactor: update CompTask page layout and integrate path_provider plugin

--- lib/application/pages/completed_task/comp_task.dart
+++ lib/application/pages/completed_task/comp_task.dart
@@ -6,39 +6,41 @@ class CompTask extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.indigo.shade300,
-          centerTitle: true,
-          title: const Text(
-            'Completed Tasks',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.indigo.shade300,
+        centerTitle: true,
+        title: const Text(
+          'Completed Tasks',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
         ),
-        body: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.only(top: 10.0, left: 10, right: 10),
-                child: Container(
-                  height: 80,
-                  decoration: BoxDecoration(
-                    color: Colors.indigo.shade100,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ListTile(
-                    title: Text(
-                      'Task $index',
-                      style: const TextStyle(
-                        decoration: TextDecoration.lineThrough,
-                      ),
-                    ),
-                    subtitle: const Text('Date: 12/12/2021'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(top: 10.0, left: 10, right: 10),
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.indigo.shade100,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ListTile(
+                title: Text(
+                  'Task $index',
+                  style: const TextStyle(
+                    decoration: TextDecoration.lineThrough,
                   ),
                 ),
-              );
-            }));
+                subtitle: const Text('Date: 12/12/2021'),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
