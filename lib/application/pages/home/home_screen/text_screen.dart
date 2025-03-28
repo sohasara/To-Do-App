4cf42a592481398ff9946153a10b1856d128ff79
Nadia Islam <sohasara853@gmail.com>
2025-03-28 16:14:53 +0600

    fix: update ndkVersion in build.gradle to a specific version

--- lib/application/pages/home/home_screen/text_screen.dart
+++ lib/application/pages/home/home_screen/text_screen.dart
@@ -16,6 +16,7 @@ class TextScreen extends StatelessWidget {
     final finishingDateController = TextEditingController();
 
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: Colors.indigo.shade50,
       appBar: AppBar(
         backgroundColor: Colors.indigo.shade300,
@@ -28,194 +29,197 @@ class TextScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: taskController,
-              decoration: InputDecoration(
-                labelText: 'Task Title',
-                labelStyle: TextStyle(color: Colors.indigo.shade900),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.indigo),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: descriptionController,
-              decoration: InputDecoration(
-                labelText: 'Description',
-                labelStyle: TextStyle(color: Colors.indigo.shade700),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.indigo),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              maxLines: 3,
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: finishingDateController,
-              readOnly: true, // Prevent manual input
-              decoration: InputDecoration(
-                labelText: 'Finishing Date',
-                hintText: 'Select a date',
-                labelStyle: TextStyle(color: Colors.indigo.shade900),
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.calendar_today),
-                  onPressed: () async {
-                    DateTime? pickedDate = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(2000),
-                      lastDate: DateTime(2100),
-                    );
-                    if (pickedDate != null) {
-                      finishingDateController.text =
-                          DateFormat('yyyy-MM-dd').format(pickedDate);
-                    }
-                  },
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.indigo),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Priority',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.indigo,
-              ),
-            ),
-            const SizedBox(height: 8),
-            Consumer(builder: (context, ref, child) {
-              final selectedPriority = ref.watch(priorityProvider);
-              return Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: ['high', 'medium', 'low'].map((priority) {
-                      return ChoiceChip(
-                        label: Text(priority.toUpperCase()),
-                        selected: selectedPriority == priority,
-                        selectedColor: Colors.indigo.shade300,
-                        backgroundColor: Colors.grey.shade300,
-                        labelStyle: TextStyle(
-                          color: selectedPriority == priority
-                              ? Colors.white
-                              : Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        onSelected: (selected) {
-                          ref.read(priorityProvider.notifier).state = priority;
-                        },
-                      );
-                    }).toList(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: taskController,
+                decoration: InputDecoration(
+                  labelText: 'Task Title',
+                  labelStyle: TextStyle(color: Colors.indigo.shade900),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.indigo),
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                  const SizedBox(height: 40),
-                  Center(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.indigo.shade300,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                controller: descriptionController,
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  labelStyle: TextStyle(color: Colors.indigo.shade700),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.indigo),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                maxLines: 3,
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: finishingDateController,
+                readOnly: true, // Prevent manual input
+                decoration: InputDecoration(
+                  labelText: 'Finishing Date',
+                  hintText: 'Select a date',
+                  labelStyle: TextStyle(color: Colors.indigo.shade900),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.calendar_today),
+                    onPressed: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2100),
+                      );
+                      if (pickedDate != null) {
+                        finishingDateController.text =
+                            DateFormat('yyyy-MM-dd').format(pickedDate);
+                      }
+                    },
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.indigo),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Priority',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.indigo,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Consumer(builder: (context, ref, child) {
+                final selectedPriority = ref.watch(priorityProvider);
+                return Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: ['high', 'medium', 'low'].map((priority) {
+                        return ChoiceChip(
+                          label: Text(priority.toUpperCase()),
+                          selected: selectedPriority == priority,
+                          selectedColor: Colors.indigo.shade300,
+                          backgroundColor: Colors.grey.shade300,
+                          labelStyle: TextStyle(
+                            color: selectedPriority == priority
+                                ? Colors.white
+                                : Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          onSelected: (selected) {
+                            ref.read(priorityProvider.notifier).state =
+                                priority;
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.indigo.shade300,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 40, vertical: 10),
                         ),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 40, vertical: 14),
-                      ),
-                      onPressed: () {
-                        final title = taskController.text.trim();
-                        final description = descriptionController.text.trim();
-                        final dateText = finishingDateController.text.trim();
+                        onPressed: () {
+                          final title = taskController.text.trim();
+                          final description = descriptionController.text.trim();
+                          final dateText = finishingDateController.text.trim();
 
-                        if (title.isEmpty ||
-                            description.isEmpty ||
-                            dateText.isEmpty) {
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              title: const Text(
-                                "Missing Fields",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.red,
+                          if (title.isEmpty ||
+                              description.isEmpty ||
+                              dateText.isEmpty) {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: const Text(
+                                  "Missing Fields",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red,
+                                  ),
                                 ),
+                                content: const Text(
+                                  "Please fill all fields before adding a task.",
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text("OK"),
+                                  ),
+                                ],
                               ),
-                              content: const Text(
-                                "Please fill all fields before adding a task.",
-                              ),
-                              actions: [
-                                TextButton(
-                                  onPressed: () => Navigator.pop(context),
-                                  child: const Text("OK"),
-                                ),
-                              ],
-                            ),
-                          );
-                          return;
-                        }
-
-                        DateTime? finishingDate;
-                        try {
-                          finishingDate = DateTime.parse(dateText);
-                        } catch (e) {
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              title: const Text("Invalid Date"),
-                              content: const Text(
-                                  "Please enter a valid date in the format YYYY-MM-DD."),
-                              actions: [
-                                TextButton(
-                                  onPressed: () => Navigator.pop(context),
-                                  child: const Text("OK"),
-                                ),
-                              ],
-                            ),
-                          );
-                          return;
-                        }
-
-                        ref.read(taskProvider.notifier).addTask(
-                              title,
-                              description,
-                              finishingDate,
-                              selectedPriority,
                             );
+                            return;
+                          }
 
-                        Navigator.pop(context);
-                      },
-                      child: const Text(
-                        'Add Task',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
+                          DateTime? finishingDate;
+                          try {
+                            finishingDate = DateTime.parse(dateText);
+                          } catch (e) {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: const Text("Invalid Date"),
+                                content: const Text(
+                                    "Please enter a valid date in the format YYYY-MM-DD."),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text("OK"),
+                                  ),
+                                ],
+                              ),
+                            );
+                            return;
+                          }
+
+                          ref.read(taskProvider.notifier).addTask(
+                                title,
+                                description,
+                                finishingDate,
+                                selectedPriority,
+                              );
+
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          'Add Task',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              );
-            }),
-          ],
+                  ],
+                );
+              }),
+            ],
+          ),
         ),
       ),
     );
