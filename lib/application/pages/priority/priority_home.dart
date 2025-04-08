b99d0d1eaa4cfb31859b6174d761792b8f8ff7ca
Nadia Islam <sohasara853@gmail.com>
2025-04-08 20:05:44 +0600

    style: add PriorityHome widget for displaying priority tasks

---
+++ lib/application/pages/priority/priority_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PriorityHome extends StatelessWidget {
+  const PriorityHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Priority Home'),
+      ),
+      body: const Center(
+        child: Text('Priority Home Page'),
+      ),
+    );
+  }
+}
